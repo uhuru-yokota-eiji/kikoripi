3496c3b79d849b8b828c3682ff28e3cb53a55269
eiji_yokota <e.yokota@uhuru.jp>
2020-03-11 11:51:08 +0900

    GPIO sample

--- api/views.py
+++ api/views.py
@@ -1,7 +1,21 @@
 from django.http.response import JsonResponse
+from grovepi import *
 
 
 def index(request):
-    print("index")
+    # print("index")
+
+    led = 2
+    pinMode(led, "OUTPUT")
+
     response = request.GET
+
+    # print(type(response))
+    # print(response)
+
+    if response.get("target") == "GPIO2" and response.get("value") == "1":
+        digitalWrite(led, 1)
+    else:
+        digitalWrite(led, 0)
+
     return JsonResponse(response)
